e0f76a80dd6493519150edd94305c67d563f02c7
HassnainMalik1 <hassnainmalik001@gmail.com>
2019-06-02 16:39:33 +0500

    Convert to MVC Pattern.

---
+++ Todony/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todony
+//
+//  Created by MalikHassnain on 02/06/2019.
+//  Copyright © 2019 MalikHassnain. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
